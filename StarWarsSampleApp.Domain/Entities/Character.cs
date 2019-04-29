b6f1df5fb702cde2ac578d0859bc825dfc97795e
Damian Taraszkiewicz <damtar94@gmail.com>
2019-04-29 13:06:42 +0200

    added entities

---
+++ StarWarsSampleApp.Domain/Entities/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWarsSampleApp.Domain.Entities
+{
+    public class Character
+    {
+        public Character()
+        {
+            Friends = new HashSet<Friendship>();
+            Episodes = new HashSet<Episode>();
+        }
+
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public ICollection<Friendship> Friends { get; set; }
+        public ICollection<Episode> Episodes { get; set; }
+    }
+}
---
+++ StarWarsSampleApp.Domain/Entities/Friendship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWarsSampleApp.Domain.Entities
+{
+    public class Friendship
+    {
+        public int CharacterId { get; set; }
+        public int FriendId { get; set; }
+
+        public virtual Character Character { get; set; }
+        public virtual Character Friend { get; set; }
+    }
+}
