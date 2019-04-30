6c371c3612a1956c68a6918ed36c95dd94807f1d
dtaraszkiewicz <damtar94@gmail.com>
2019-04-30 14:30:20 +0200

    setup swagger and renamed api folder (#10)

--- StarWarsSampleApp.Api/Controllers/EpisodesController.cs
+++ StarWarsSampleApp.Api/Controllers/EpisodesController.cs
@@ -7,18 +7,37 @@ using StarWarsSampleApp.Application.Episodes.Commands.CreateEpisode;
 
 namespace StarWarsSampleApp.Api.Controllers
 {
+    /// <summary>
+    /// Episodes API
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class EpisodesController : BaseController
     {
-        // GET api/episodes
+        /// <summary>
+        /// Gets all episodes
+        /// </summary>
+        /// <returns>List of Episode View Model</returns>
         [HttpGet]
         public async Task<ActionResult<IList<EpisodeViewModel>>> Get()
         {
             return Ok(await Mediator.Send(new GetEpisodesQuery()));
         }
 
-        // POST api/episodes
+        /// <summary>
+        /// Creates a new episode
+        /// </summary>
+        /// <remarks>
+        /// Sample create Episode:
+        /// 
+        ///     POST api/episodes
+        ///     {
+        ///         "name": "episode name"
+        ///     }
+        ///     
+        /// </remarks>
+        /// <param name="command">Episode to create</param>
+        /// <returns>The created episode id</returns>
         [HttpPost]
         public async Task<ActionResult<int>> Create([FromBody] CreateEpisodeCommand command)
         {
