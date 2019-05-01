6e6d69995d931ee962a6d2e1d383db712a3ef5be
Damian Taraszkiewicz <damtar94@gmail.com>
2019-05-01 09:35:15 +0200

    added first passing test, added xUnit, Respawn and Shouldly

---
+++ StarWarsSampleApp.Tests/Episodes/Queries/GetEpisodesQueryHandlerTest.cs
@@ -0,0 +1,38 @@
+﻿using StarWarsSampleApp.Tests.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using Respawn;
+using Shouldly;
+using StarWarsSampleApp.Domain.Entities;
+using Xunit;
+
+namespace StarWarsSampleApp.Tests.Episodes.Queries
+{
+    [Collection("Test collection")]
+    public class GetEpisodesQueryHandlerTest : IAsyncLifetime
+    {
+        private readonly TestFixture _testFixture;
+        private readonly Checkpoint _checkpoint;
+
+        public GetEpisodesQueryHandlerTest(TestFixture fixture)
+        {
+            _testFixture = fixture;
+            _checkpoint = new Checkpoint();
+        }
+
+        public Task InitializeAsync() => _checkpoint.Reset(_testFixture.ConnectionString);
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        [Fact]
+        public async Task firstTest()
+        {
+            _testFixture.Context.Episodes.Add(new Episode {Name = "test"});
+            await _testFixture.Context.SaveChangesAsync();
+
+            var episode = _testFixture.Context.Episodes.FirstOrDefault();
+
+            episode.Name.ShouldBe("test");
+        }
+    }
+}
---
+++ StarWarsSampleApp.Tests/Infrastructure/TestFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using StarWarsSampleApp.Persistence;
+using Xunit;
+
+namespace StarWarsSampleApp.Tests.Infrastructure
+{
+    public class TestFixture
+    {
+        public StarWarsSampleAppDbContext Context { get; }
+
+        public IMapper Mapper { get; }
+
+        public string ConnectionString { get; }
+
+        public TestFixture()
+        {
+            ConnectionString = GetConnectionString();
+            Context = GetDbContext();
+            Mapper = AutoMapperFactory.Create();
+        }
+
+        private StarWarsSampleAppDbContext GetDbContext()
+        {
+            var builder = new DbContextOptionsBuilder<StarWarsSampleAppDbContext>();
+            builder.UseSqlServer(ConnectionString);
+
+            var dbContext = new StarWarsSampleAppDbContext(builder.Options);
+
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+
+        private string GetConnectionString()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional:false)
+                .AddEnvironmentVariables()
+                .Build()
+                .GetConnectionString("StarWarsSampleAppTestDatabase");
+        }
+
+        [CollectionDefinition("Test collection")]
+        public class TestCollection : ICollectionFixture<TestFixture>
+        {
+            // This class has no code, and is never created. Its purpose is simply
+            // to be the place to apply [CollectionDefinition] and all the
+            // ICollectionFixture<> interfaces.
+        }
+    }
+}
