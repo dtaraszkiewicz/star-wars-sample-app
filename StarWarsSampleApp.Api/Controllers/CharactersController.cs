f2dc2b27a3aa53ca07faaa78bd4a86eca1c4ba66
Damian Taraszkiewicz <damtar94@gmail.com>
2019-05-02 12:31:57 +0200

    get character query

---
+++ StarWarsSampleApp.Api/Controllers/CharactersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarWarsSampleApp.Application.Characters.Queries;
+
+namespace StarWarsSampleApp.Api.Controllers
+{
+    /// <summary>
+    /// Episodes API
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharactersController : BaseController
+    {
+        /// <summary>
+        /// Get character by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Character View Model</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetCharacterViewModel>> Get(int id)
+        {
+            return Ok(await Mediator.Send(new GetCharacterQuery {Id = id}));
+        }
+    }
+}
---
+++ StarWarsSampleApp.Application/Characters/Queries/GetCharacterQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace StarWarsSampleApp.Application.Characters.Queries
+{
+    public class GetCharacterQuery : IRequest<GetCharacterViewModel>
+    {
+        public int Id { get; set; }
+    }
+}
