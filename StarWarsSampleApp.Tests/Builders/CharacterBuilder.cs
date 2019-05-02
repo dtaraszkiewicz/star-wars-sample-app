ab274869265bafbed2f6be71af5912477e2dba28
Damian Taraszkiewicz <damtar94@gmail.com>
2019-05-02 12:54:03 +0200

    test coverage

---
+++ StarWarsSampleApp.Tests/Builders/CharacterBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Bogus;
+using StarWarsSampleApp.Domain.Entities;
+using StarWarsSampleApp.Persistence;
+
+namespace StarWarsSampleApp.Tests.Builders
+{
+    public class CharacterBuilder
+    {
+        private readonly Faker<Character> _characterFaker;
+        private List<Character> _characters;
+
+        public CharacterBuilder()
+        {
+            _characterFaker = new Faker<Character>()
+                .StrictMode(false)
+                .CustomInstantiator(e => new Character())
+                .RuleFor(x => x.Name, s => s.Name.FullName());
+        }
+
+        public CharacterBuilder Generate(int amount = 1)
+        {
+            _characters = _characterFaker.Generate(amount);
+            return this;
+        }
+
+        public CharacterBuilder SaveChanges(StarWarsSampleAppDbContext context)
+        {
+            context.Characters.AddRange(_characters);
+            context.SaveChanges();
+            return this;
+        }
+
+        public List<Character> Build()
+        {
+            return _characters;
+        }
+    }
+}
